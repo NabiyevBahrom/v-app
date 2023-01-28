d602cd83359b14314352cb70c16b0bde1120cd71
Bahrom <113708211+NabiyevBahrom@users.noreply.github.com>
2023-01-28 21:18:54 +0500

    Update WeatherView.vue

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -87,13 +87,13 @@ export default {
     });
 
     function requestApi(city) {
-      api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+      api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f1ca80245b8cd906e9dc9211dd5a8571`;
       fetchData();
     }
 
     function onSuccess(position) {
       const { latitude, longitude } = position.coords; // getting lat and lon of the user device from coords obj
-      api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+      api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=f1ca80245b8cd906e9dc9211dd5a8571`;
       fetchData();
     }
 
